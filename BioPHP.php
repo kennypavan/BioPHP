9f792ee097d2fc52e4ced447b72d8b963dccf666
Kenny <kenpavan@gmail.com>
2017-08-04 16:09:11 -0700

    Add a blank class to get started

---
+++ BioPHP.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * BioPHP
+ *
+ * @category   BioInformatics
+ * @package    BioPHP
+ * @author     Kenny Pavan  <kenpavan@gmail.com>
+ * @license    MIT
+ */
+
+class BioPHP {
+
+
+	public function DnaToRna()
+ 	{
+
+		return $result;
+
+	}
+}
+
+?>
+
+
