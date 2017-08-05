b79d111578f6df71bfff47c6b5a31a2ee5bf5afa
Kenny <kenpavan@gmail.com>
2017-08-05 02:09:21 -0700

    Started with very basic functionality in BioPHP class.

--- BioPHP.php
+++ BioPHP.php
@@ -12,14 +12,51 @@
 class BioPHP {
 
 
-	public function DnaToRna()
- 	{
+	public $sequenceA;
+	public $sequenceB;
+	public $sequenceC;
 
-		return $result;
+
+	public function __construct($sequenceA=false, $sequenceB=false, $sequenceC=false)
+	{
+		$this->sequenceA = $sequenceA;
+		$this->normalizeSequence();
+	}
+
+
+	public function normalizeSequence()
+	{
+
+ 		$this->sequenceA = strtoupper($this->sequenceA);
+		return $this;
 
 	}
+
+
+	public function reverseSequence()
+ 	{
+
+ 		$this->sequenceA = strrev($this->sequenceA);
+		return $this;
+
+	}
+
+	public function complementDnaSequence()
+	{
+
+		$this->sequenceA = str_replace("A", "t", $this->sequenceA);
+		$this->sequenceA = str_replace("T", "a", $this->sequenceA);
+		$this->sequenceA = str_replace("G", "c", $this->sequenceA);
+		$this->sequenceA = str_replace("C", "g", $this->sequenceA);
+		$this->normalizeSequence();
+		return $this;
+
+	}
+
 }
 
+$BioPHP = new BioPHP('ATGAAa');
+$BioPHP->reverseSequence();
+$BioPHP->complementDnaSequence();
+echo $BioPHP->sequenceA;
 ?>
-
-
